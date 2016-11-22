3b804faf5ac7701b28d5c8ba96b94c6db3392319
Ralph Regalado <rv.regalado@gmail.com>
2016-11-22 16:03:47 +0800

    Change config

--- codelabs/includes/scripts/config.js
+++ codelabs/includes/scripts/config.js
@@ -1,8 +1,7 @@
-// Initialize Firebase
 var config = {
-	apiKey: "",
- 	authDomain: "",
-	databaseURL: "",
-	storageBucket: ""
-};
-firebase.initializeApp(config);
+   apiKey: "AIzaSyCeYsuRNmqoAtDa3k8oDohy8hflzCsc_IY",
+   authDomain: "gdg-devfest-philippines-2016.firebaseapp.com",
+   databaseURL: "https://gdg-devfest-philippines-2016.firebaseio.com",
+   storageBucket: "",
+ };
+ firebase.initializeApp(config);
