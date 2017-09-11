813eb3434ceb5885d599181dfefeaafa5f93810b
Toni-Jan Keith Monserrat <tonijanmonserrat@gmail.com>
2017-09-11 16:27:45 +0800

    added speakers page
    Signed-off-by: Toni-Jan Keith Monserrat <tonijanmonserrat@gmail.com>

--- src/routing.js
+++ src/routing.js
@@ -3,6 +3,7 @@
       '/': () => { return import(/* webpackChunkName: "devfest-landing-page" */ './modules/devfest-module/pages/devfest-landing-page/devfest-landing-page.js') }, 
 '/index.html': () => { return import(/* webpackChunkName: "devfest-landing-page" */ './modules/devfest-module/pages/devfest-landing-page/devfest-landing-page.js') }, 
 '/speakers': () => { return import(/* webpackChunkName: "devfest-speakers-page" */ './modules/devfest-module/pages/devfest-speakers-page/devfest-speakers-page.js') }, 
+'/speakers/:id': () => { return import(/* webpackChunkName: "devfest-speaker-page" */ './modules/devfest-module/pages/devfest-speaker-page/devfest-speaker-page.js') }, 
 '/tickets': () => { return import(/* webpackChunkName: "devfest-tickets-page" */ './modules/devfest-module/pages/devfest-tickets-page/devfest-tickets-page.js') }, 
 '/auth-check': () => { return import(/* webpackChunkName: "devfest-authorized-page" */ './modules/devfest-module/pages/devfest-authorized-page/devfest-authorized-page.js') }
     }
