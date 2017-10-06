0a368bfe09fb6eb832570d2029cb0b9742f07ad5
Toni-Jan Keith Monserrat <tonijanmonserrat@gmail.com>
2017-10-06 20:58:13 +0800

    added sponsors
    Signed-off-by: Toni-Jan Keith Monserrat <tonijanmonserrat@gmail.com>

---
+++ src/modules/devfest-module/pages/devfest-sponsors-page/devfest-sponsors-page.js
@@ -0,0 +1,59 @@
+import 'polymer/polymer.html'
+import 'paper-ripple/paper-ripple.html'
+import 'iron-flex-layout/iron-flex-layout.html'
+import 'shadycss/apply-shim.html'
+import 'iron-icon/iron-icon.html'
+import 'marked-element/marked-element.html'
+import '../../fonts/devfest-fonts.html'
+import './devfest-sponsors-page.html'
+import '../../components/devfest-footer/devfest-footer.js'
+import contentLoaderMixin from '../../../content-loader/content-loader-mixin.js'
+import marked from 'marked'
+import firebaseConfig from '../../../../firebase.js'
+import app from '../../../../app.js'
+window.marked = window.marked || marked
+
+class DevfestSponsorsPage extends contentLoaderMixin(Polymer.Element) {
+  static get is () { return 'devfest-sponsors-page' }
+
+  static get properties () {
+    return {
+      banner: {
+        type: String
+      },
+      about: {
+        type: String
+      },
+      aboutImage: {
+        type: String
+      },
+      expect: {
+        type: Array,
+        value: []
+      },
+      speakers: {
+        type: Array,
+        value: []
+      }
+    }
+  }
+
+  constructor () {
+    super()
+    this._apiKey = firebaseConfig[0].apiKey
+    this._app = app
+  }
+
+  connectedCallback () {
+    super.connectedCallback()
+    this.reload()
+  }
+
+  reload() {
+    this._fetchContent('pages/sponsors.md')
+  }
+}
+
+window.customElements.define(DevfestSponsorsPage.is, DevfestSponsorsPage)
+
+export default DevfestSponsorsPage
