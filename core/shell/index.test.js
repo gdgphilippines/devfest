25954473d154a74fd18463325a99a6ffb71bb78c
Toni-Jan Keith Monserrat <tonijanmonserrat@gmail.com>
2017-10-07 18:56:15 +0800

    fixed
    Signed-off-by: Toni-Jan Keith Monserrat <tonijanmonserrat@gmail.com>

--- core/shell/index.test.js
+++ core/shell/index.test.js
@@ -1,6 +1,5 @@
 const expect = require('chai').expect
 const shell = require('./index')
-// const page = require('./../../src/modules/devfest-module/pages/devfest-landing-page/devfest-landing-page')
 
 describe('App shell', () => {
 
@@ -9,21 +8,7 @@ describe('App shell', () => {
   })
 
   it('should work', () => {
-    document.createElement('div')
     const el = document.createElement('app-shell')
     expect(el.constructor.is).to.equal('app-shell')
   })
 })
-
-// describe('Devfest landing page', () => {
-
-//   it('should exist', () => {
-//     expect(page).to.exist
-//   })
-
-//   it('should work', () => {
-//     document.createElement('div')
-//     const el = document.createElement('devfest-landing-page')
-//     expect(el.constructor.is).to.equal('devfest-landing-page')
-//   })
-// })
--- src/modules/devfest-module/pages/devfest-landing-page/devfest-landing-page.test.js
+++ src/modules/devfest-module/pages/devfest-landing-page/devfest-landing-page.test.js
@@ -1,14 +1,13 @@
-const expect = require('chai').expect
-const page = require('./devfest-landing-page')
+import { expect } from 'chai'
+import page from './devfest-landing-page'
 
-discribe('Devfest landing page', () => {
-	
+describe('Devfest landing page', () => {
+
 	it('should exist', () => {
 		expect(page).to.exist
 	})
 
 	it('should work', () => {
-		document.createElement('div')
 		const el = document.createElement('devfest-landing-page')
 		expect(el.constructor.is).to.equal('devfest-landing-page')
 	})
