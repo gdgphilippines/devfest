705c62b3883a9aa3619deca79ec6e0515cbe2c4a
Rokuto <gauven.boro@gmail.com>
2017-10-10 02:22:43 -0400

    Add devfest-authorized-page.test.js and Update test.js

--- gulp/test.js
+++ gulp/test.js
@@ -1,2 +1,3 @@
 import '../core/shell/index.test'
 import '../src/modules/devfest-module/pages/devfest-landing-page/devfest-landing-page.test'
+import '../src/modules/devfest-module/pages/devfest-authorized-page/devfest-authorized-page.test'
