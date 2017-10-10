b39ce3c7a0396f4b05bdfb2ef41f835ccdc6e520
Rokuto <gauven.boro@gmail.com>
2017-10-10 02:24:50 -0400

    Update test.js

--- gulp/test.js
+++ gulp/test.js
@@ -1,3 +1,4 @@
 import '../core/shell/index.test'
 import '../src/modules/devfest-module/pages/devfest-landing-page/devfest-landing-page.test'
 import '../src/modules/devfest-module/pages/devfest-authorized-page/devfest-authorized-page.test'
+import '../src/modules/devfest-module/pages/devfest-not-authorized-page/devfes-not-authorized-page.test'
